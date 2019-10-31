3818679a25c052478df093c372aa58463d44b43b
BuildTools <jmisiti@student.42.fr>
2019-10-31 17:15:10 +0100

    Pushing example

---
+++ src/author/servername/pluginname/main.java
@@ -0,0 +1,15 @@
+package author.servername.pluginname;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    	
+    }
+    
+    @Override
+    public void onDisable() {
+    	
+    }
+}
